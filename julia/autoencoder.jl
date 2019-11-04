0916b33f8754f8ff76949ca1842e26a04212c809
Hebi Li <lihebi.com@gmail.com>
2019-11-03 19:49:09 -0600

    add vae

--- julia/autoencoder.jl
+++ julia/autoencoder.jl
@@ -192,3 +192,39 @@ function AdvAE()
     evaluate_attack(Chain(ae, cnn), attack_PGD_k(40), trainX, trainY, testX, testY)
     evaluate_attack(cnn, attack_PGD_k(40), trainX, trainY, testX, testY)
 end
+
+# Extend distributions slightly to have a numerically stable logpdf for `p` close to 1 or 0.
+logpdf(b::Bernoulli, y::Bool) = y * log(b.p + eps(Float32)) + (1f0 - y) * log(1 - b.p + eps(Float32))
+
+function vae()
+    # Latent dimensionality, # hidden units.
+    Dz, Dh = 5, 500
+    # Components of recognition model / "encoder" MLP.
+    A, μ, logσ = Dense(28^2, Dh, tanh), Dense(Dh, Dz), Dense(Dh, Dz)
+    g(X) = (h = A(X); (μ(h), logσ(h)))
+    z(μ, logσ) = μ + exp(logσ) * randn(Float32)
+    # Generative model / "decoder" MLP.
+    f = Chain(Dense(Dz, Dh, tanh), Dense(Dh, 28^2, σ))
+
+    # KL-divergence between approximation posterior and N(0, 1) prior.
+    kl_q_p(μ, logσ) = 0.5f0 * sum(exp.(2f0 .* logσ) + μ.^2 .- 1f0 .- (2 .* logσ))
+    # logp(x|z) - conditional probability of data given latents.
+    logp_x_z(x, z) = sum(logpdf.(Bernoulli.(f(z)), x))
+    # Monte Carlo estimator of mean ELBO using M samples.
+    L̄(X) = ((μ̂, logσ̂) = g(X); (logp_x_z(X, z.(μ̂, logσ̂)) - kl_q_p(μ̂, logσ̂)) * 1 // M)
+
+    loss(X) = -L̄(X) + 0.01f0 * sum(x->sum(x.^2), Flux.params(f))
+
+    # Sample from the learned model.
+    modelsample() = rand.(Bernoulli.(f(z.(zeros(Dz), zeros(Dz)))))
+
+
+    evalcb = throttle(() -> @show(-L̄(X[:, rand(1:N, M)])), 30)
+    opt = ADAM()
+    ps = Flux.params(A, μ, logσ, f)
+
+    @progress for i = 1:20
+        @info "Epoch $i"
+        Flux.train!(loss, ps, zip(data), opt, cb=evalcb)
+    end
+end

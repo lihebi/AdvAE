9f8d99290cc71970134d5fedc76e021f26d2d984
Hebi Li <lihebi.com@gmail.com>
2019-11-10 23:07:34 +0000

    use my_parse_record and remove mean subtract, to maintain [0,255]

--- tf2/data_utils.py
+++ tf2/data_utils.py
@@ -75,24 +75,40 @@ def download_cifar():
 import sys
 sys.path.append('./tf_models')
 from official.vision.image_classification import cifar_preprocessing
+
+def my_parse_record(raw_record, is_training, dtype):
+    record_vector = tf.io.decode_raw(raw_record, tf.uint8)
+    label = tf.cast(record_vector[0], tf.int32)
+    depth_major = tf.reshape(record_vector[1:32*32*3+1],
+                             [3, 32, 32])
+    image = tf.cast(tf.transpose(a=depth_major, perm=[1, 2, 0]), tf.float32)
+
+    image = my_preprocess_image(image, is_training)
+    image = tf.cast(image, dtype)
+    label = tf.compat.v1.sparse_to_dense(label, (10,), 1)
+    return image, label
+
+
+def my_preprocess_image(image, is_training):
+    if is_training:
+        image = tf.image.resize_with_crop_or_pad(
+            image, 32 + 8, 32 + 8)
+        image = tf.image.random_crop(image, [32, 32, 3])
+        image = tf.image.random_flip_left_right(image)
+
+    # Subtract off the mean and divide by the variance of the pixels.
+    # DEBUG remove standardization, so that range is 0-255
+    # image = tf.image.per_image_standardization(image)
+    return image
+
 def load_cifar10_ds(batch_size):
-    # , train_epochs
     data_dir = 'cache/cifar-10-batches-bin'
     ds = cifar_preprocessing.input_fn(
         is_training=True,
-        # data_dir='/home/user/.keras/datasets/cifar-10-batches-py',
         data_dir=data_dir,
         batch_size=batch_size,
-        # FIXME num_epochs does not seem to be used. When is_training is True,
-        # it is repeated, otherwise it is not.
-        #
-        # num_epochs=train_epochs,
-        parse_record_fn=cifar_preprocessing.parse_record,
-        # datasets_num_private_threads=flags_obj.datasets_num_private_threads,
-        # dtype=dtype,
-        # Setting drop_remainder to avoid the partial batch logic in normalization
-        # layer, which triggers tf.where and leads to extra memory copy of input
-        # sizes between host and GPU.
+        # parse_record_fn=cifar_preprocessing.parse_record,
+        parse_record_fn=my_parse_record,
         drop_remainder=True)
 
     # test ds
@@ -101,9 +117,8 @@ def load_cifar10_ds(batch_size):
         data_dir=data_dir,
         batch_size=batch_size,
         # FIXME should this be 1?
-        # num_epochs=train_epochs,
-        num_epochs=1,
-        parse_record_fn=cifar_preprocessing.parse_record)
+        # num_epochs=1,
+        parse_record_fn=my_parse_record)
     steps_per_epoch = cifar_preprocessing.NUM_IMAGES['train'] // batch_size
     test_steps_per_epoch = cifar_preprocessing.NUM_IMAGES['validation'] // batch_size
 
@@ -125,7 +140,7 @@ def sample_and_view(x, num=10):
         # rescale to 2 because it is a bit small
         img = img.resize((img.width*3, img.height*3), Image.ANTIALIAS)
         img.save(tmp)
-    elif train_x.shape[1] == 32:
+    elif x.shape[1] == 32:
         # FIXME how to scale it?
         plt.imsave(tmp, stacked, dpi=500)
     print('#<Image: ' + tmp.name + '>')
@@ -142,3 +157,7 @@ def test():
     ds, test_ds, steps_per_epoch, test_steps_per_epoch = load_cifar10_ds(50)
     for i, d in enumerate(test_ds):
         print(i)
+
+    x, y = next(iter(ds))
+    # FIXME this is not [0,1]
+    sample_and_view(x/255)

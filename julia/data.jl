c0396f18b0ccd7f50df82a713b4a02c97a771d39
Hebi Li <lihebi.com@gmail.com>
2019-11-25 17:44:00 +0000

    lower memory setting for 1070

--- julia/data.jl
+++ julia/data.jl
@@ -5,9 +5,9 @@
 # FIXME does not work
 #
 # For 1070
-# g=7.5
+g=7.0
 # For 2080 Ti
-g=9.0
+# g=9.0
 ENV["CUARRAYS_MEMORY_LIMIT"] = convert(Int, round(g * 1024 * 1024 * 1024))
 
 # this manually set works
